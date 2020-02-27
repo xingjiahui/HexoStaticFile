563cbcb9103c9e82da57286f50bb727135757bad
Antmoe <sviptzk@163.com>
2020-02-27 20:01:53 +0800

    修改样式

--- HomePage/assets/js/main.js
+++ HomePage/assets/js/main.js
@@ -1,1 +1,8 @@
-console.log("\n %c Made with love %c by AyagawaSeirin | qwq.best ","color:#444;background:#eee;padding:5px 0;","color:#F8F8FF;background:#F4A7B9;padding:5px 0;");
+console.log(
+  "\n %c Made with love %c by Antmoe | www.antmoe.com ",
+  "color:#444;background:#eee;padding:5px 0;",
+  "color:#F8F8FF;background:#F4A7B9;padding:5px 0;"
+);
+num = Math.round(Math.random()*10);
+imgurl = 'https://cdn.jsdelivr.net/gh/sviptzk/HexoStaticFile@latest/HomePage/assets/img/background'+ num+'.jpg'
+document.getElementById('rin-bg').style.backgroundImage = "url("+imgurl+")";
