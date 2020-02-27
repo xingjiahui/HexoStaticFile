3bd69341428dc4c064f52944ec29fef8a5cfb096
Antmoe <sviptzk@163.com>
2020-02-27 21:17:54 +0800

    提交修改

--- HomePage/assets/js/main.js
+++ HomePage/assets/js/main.js
@@ -3,6 +3,6 @@ console.log(
   "color:#444;background:#eee;padding:5px 0;",
   "color:#F8F8FF;background:#F4A7B9;padding:5px 0;"
 );
-num = Math.round(Math.random()*10);
-imgurl = 'https://cdn.jsdelivr.net/gh/sviptzk/HexoStaticFile@latest/HomePage/assets/img/background'+ num+'.jpg'
-document.getElementById('rin-bg').style.backgroundImage = "url("+imgurl+")";
+// num = Math.round(Math.random()*10);
+// imgurl = 'https://cdn.jsdelivr.net/gh/sviptzk/HexoStaticFile@latest/HomePage/assets/img/background'+ num+'.jpg'
+// document.getElementById('rin-bg').style.backgroundImage = "url("+imgurl+")";
