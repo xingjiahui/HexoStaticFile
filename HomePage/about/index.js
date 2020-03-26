33079bff768fe614d199fee1d11f712c71ab828c
零度冷咖啡 <sviptzk@163.com>
2020-03-26 18:47:30 +0800

    关于我

---
+++ HomePage/about/index.js
@@ -0,0 +1,40 @@
+var windowWidth = $(window).width();
+if (location.href.indexOf(atob("YWJvdXQuYW50bW9lLmNvbQ==")) == -1) {
+  location.href = atob("aHR0cHMlM0EvL2Fib3V0LmFudG1vZS5jb20=");
+}
+if (windowWidth < 767) {
+  // do something
+  $("#aplayer").remove();
+}
+console.log(
+  "\n" +
+    " %c PersonalWebSite by Dreamy.TZK " +
+    " %c https://www.antmoe.com " +
+    "\n" +
+    "\n",
+  "color: #fff; background: #fd79a8; padding:5px 0;",
+  "background: #FFF; padding:5px 0;"
+);
+//弹出一个页面层
+$("#kaygb-blog").on("click", function() {
+  layer.open({
+    type: 2,
+    title: "小康博客",
+    shadeClose: true,
+    shade: false,
+    maxmin: true, //开启最大化最小化按钮
+    area: ["80%", "100%"],
+    content: "//www.antmoe.com/"
+  });
+});
+$("#kaygb-mail").on("click", function() {
+  layer.open({
+    type: 2,
+    title: "向我发送E-mail",
+    shadeClose: true,
+    shade: false,
+    maxmin: true, //开启最大化最小化按钮
+    area: ["80%", "100%"],
+    content: "//mail.qq.com/cgi-bin/qm_share?t=qm_mailme&email=sviptzk@163.com"
+  });
+});
