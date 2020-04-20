691b5926e2d57afa678d3ec183f7f09b32841373
零度冷咖啡 <sviptzk@163.com>
2020-04-20 21:33:01 +0800

    添加了评论多个表情 删掉了表情库文件 修改了部分样式

--- Hexo/emotion/image/py.py
+++
@@ -1,9 +0,0 @@
-import glob
-imagelist= sorted(glob.glob('*.png'))
-print(imagelist)
-for i in imagelist:
-    i = i.replace(".png","")
-    print('''{
-    "icon": "'''+i+'''",
-    "text": "表情"
-        },''')
