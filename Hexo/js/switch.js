1b359a2b0fd301038b40eab41ded67e4968c9167
零度冷咖啡 <sviptzk@163.com>
2020-04-27 12:39:53 +0800

    更新版本

--- Hexo/js/switch.js
+++ Hexo/js/switch.js
@@ -1,30 +1,31 @@
-$("#switch-comment").click(function () {
-  switchComment();
-  return false;
-});
 function switchComment() {
-  const title =
-    $("#switch-comment").attr("title") === "切换为Gitalk"
-      ? "切换为Valine"
-      : "切换为Gitalk";
-  const i = $("#switch-comment>i");
-  if ($("#gitalk-container").css("display") === "none") {
-    $("#vcomment").slideUp("normal", () => {
-      $("#gitalk-container").slideDown("normal", () => {
-        $("#switch-comment").attr("title", title);
-        i.hasClass("fa-toggle-off")
-          ? i.removeClass("fa-toggle-off").addClass("fa-toggle-on")
-          : i.removeClass("fa-toggle-on").addClass("fa-toggle-off");
+  const t =
+      "切换为Gitalk" === $("#switch-comment").attr("title")
+        ? "切换为Valine"
+        : "切换为Gitalk",
+    o = $("#switch-comment>i");
+  "none" === $("#gitalk-container").css("display")
+    ? $("#vcomment").slideUp("normal", () => {
+        $("#gitalk-container").slideDown("normal", () => {
+          $("#switch-comment").attr("title", t),
+            o.hasClass("fa-toggle-off")
+              ? o.removeClass("fa-toggle-off").addClass("fa-toggle-on")
+              : o.removeClass("fa-toggle-on").addClass("fa-toggle-off");
+        });
+      })
+    : $("#gitalk-container").slideUp("normal", () => {
+        $("#vcomment").slideDown("normal", () => {
+          $("#switch-comment").attr("title", t),
+            o.hasClass("fa-toggle-off")
+              ? o.removeClass("fa-toggle-off").addClass("fa-toggle-on")
+              : o.removeClass("fa-toggle-on").addClass("fa-toggle-off");
+        });
       });
-    });
-  } else {
-    $("#gitalk-container").slideUp("normal", () => {
-      $("#vcomment").slideDown("normal", () => {
-        $("#switch-comment").attr("title", title);
-        i.hasClass("fa-toggle-off")
-          ? i.removeClass("fa-toggle-off").addClass("fa-toggle-on")
-          : i.removeClass("fa-toggle-on").addClass("fa-toggle-off");
-      });
-    });
-  }
 }
+var commentElement = document.getElementsByClassName("comment_headling")[0];
+commentElement.innerHTML +=
+  '<a id="switch-comment" href="javascript:void(0);" title="切换为Gitalk" target="_self"><i class="fa fas fa-toggle-off" aria-hidden="true"></i></a>';
+document.getElementById("gitalk-container").style.display = "none";
+$("#switch-comment").click(function () {
+  return switchComment(), !1;
+});
