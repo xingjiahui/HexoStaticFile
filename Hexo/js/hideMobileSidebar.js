993c8d396b35be40d0d6c4a39bf90e4eb5ed4c91
零度冷咖啡 <sviptzk@163.com>
2020-05-03 21:14:43 +0800

    修改默认隐藏手机侧边栏菜单

---
+++ Hexo/js/hideMobileSidebar.js
@@ -0,0 +1,6 @@
+var menus_item_child = document.getElementsByClassName("menus_item_child");
+var menus_expand = document.getElementsByClassName("menus-expand");
+for (var i = 0; i < menus_item_child.length; i++) {
+  menus_item_child[i].style.display = "none";
+  menus_expand[i].className += " menus-closed";
+}
